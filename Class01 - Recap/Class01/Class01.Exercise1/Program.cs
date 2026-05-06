f173e2f19ce30a9e8470e1288c76dc2f9ee013c5
Animrymic <trojanovikdragan@gmail.com>
2026-05-06 19:19:58 +0200

    Class01 - Exercise 1 added

---
+++ Class01 - Recap/Class01/Class01.Exercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿List<string> names = new List<string>();
+string input;
+
+Console.WriteLine("Please enter names (Press x to stop): ");
+
+while (true)
+{
+    input = Console.ReadLine(); 
+
+    if(input.ToLower() == "x")
+    {
+        break;
+    }
+
+    if (!string.IsNullOrWhiteSpace(input))
+        names.Add(input); 
+}
+
+Console.WriteLine("Enter a text: ");
+string text = Console.ReadLine();
+
+Console.WriteLine("\n Occurances: ");
+
+foreach (var name in names)
+{
+    int count = 0;
+
+    string lowerText = text.ToLower();
+    string lowerName = name.ToLower();
+
+    int index = 0;
+
+    while ((index = lowerText.IndexOf(lowerName, index)) != -1)
+    {
+        count++;
+        index += lowerName.Length; 
+    }
+
+    Console.WriteLine($"{name}: {count}");
+}
