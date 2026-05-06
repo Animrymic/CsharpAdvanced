6ea8af5917b0b2f67a5ff25fbbacc2f38052c084
Animrymic <trojanovikdragan@gmail.com>
2026-05-06 18:28:03 +0200

    Class01 - Recap added

---
+++ Class01 - Recap/Class01/Class01.Examples/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StaticRecap
+{
+    public class Developer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Developer()
+        {
+            
+        }
+    }
+}
---
+++ Class01 - Recap/Class01/Class01.Recap/Program.cs
@@ -0,0 +1,37 @@
+﻿List<string> names = new List<string>();
+
+Console.WriteLine("Enter names (type 'x' to stop):");
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "x")
+        break;
+
+    names.Add(input);
+}
+
+Console.WriteLine("Enter a text:");
+string text = Console.ReadLine();
+
+string lowerText = text.ToLower();
+
+Console.WriteLine("\nResults:");
+
+foreach (var name in names)
+{
+    string lowerName = name.ToLower();
+    int count = 0;
+    int index = 0;
+
+    while ((index = lowerText.IndexOf(lowerName, index)) != -1)
+    {
+        count++;
+        index += lowerName.Length;
+    }
+
+    Console.WriteLine($"{name} appears {count} times.");
+}
+
+Console.WriteLine();
