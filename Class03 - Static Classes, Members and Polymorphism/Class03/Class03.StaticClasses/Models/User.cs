d740ab3449b9a43e976619ee99ba9e3014942b85
Animrymic <trojanovikdragan@gmail.com>
2026-05-11 20:47:29 +0200

    - Class03 final final demo code added

--- Class03 - Static Classes, Members and Polymorphism/Class03/Class03.StaticClasses/Models/User.cs
+++ Class03 - Static Classes, Members and Polymorphism/Class03/Class03.StaticClasses/Models/User.cs
@@ -5,7 +5,19 @@ namespace Class03.StaticClasses.Models;
 public class User : BaseEntity
 {
     //public int Id { get; set; }
-    public string Username { get; set; }
+    private string username; 
+    public string Username
+    {
+        get { return username;  }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("Username cannot be empty");
+            }
+            username = value;
+        }
+    }
     public string Address { get; set; }
     public List<Order> Orders { get; set; } = new List<Order>();
 
