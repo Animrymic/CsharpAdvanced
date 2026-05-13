6bda586ef53d0f9bce9d756fa5b2e868e017231c
Animrymic <trojanovikdragan@gmail.com>
2026-05-13 20:39:11 +0200

    -- class04 extension layout added

--- Class04 - Generics and Extension Methods/Class04/Class04.Extensions/Helpers/ListHelper.cs
+++ Class04 - Generics and Extension Methods/Class04/Class04.Extensions/Helpers/ListHelper.cs
@@ -1,6 +1,12 @@
 ﻿namespace Class04.Extensions.Helpers;
 
-public class ListHelper
+public static class ListHelper
 {
-
+    public static void PrintItems<T>(List<T> items)
+    {
+        foreach (T item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
 }
--- Class04 - Generics and Extension Methods/Class04/Class04.Extensions/Program.cs
+++ Class04 - Generics and Extension Methods/Class04/Class04.Extensions/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Class04.Extensions.Helpers;
+
+List<int> integers = new List<int>() { 1, 2, 3, 4, 5 }; 
+List<string> strings = ["str1", "str2", "str3"];
+
+ListHelper.PrintItems<int>(integers); 
