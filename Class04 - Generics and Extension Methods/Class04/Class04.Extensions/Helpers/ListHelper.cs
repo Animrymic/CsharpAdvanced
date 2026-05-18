a99aa3ad54cd366d08138b2bfe181fbc4f688e22
Animrymic <trojanovikdragan@gmail.com>
2026-05-18 18:01:18 +0200

    - extenstions added

--- Class04 - Generics and Extension Methods/Class04/Class04.Extensions/Helpers/ListHelper.cs
+++ Class04 - Generics and Extension Methods/Class04/Class04.Extensions/Helpers/ListHelper.cs
@@ -2,11 +2,17 @@
 
 public static class ListHelper
 {
-    public static void PrintItems<T>(List<T> items)
+    public static void PrintItems<T>(this List<T> items)
     {
         foreach (T item in items)
         {
             Console.WriteLine(item);
         }
+    }
+
+    public static void PrintListInfo<T>(this List<T> items)
+    {
+        string listType = typeof(T).Name;
+        Console.WriteLine($"This list has {items.Count} and is of type {listType}.");
     }
 }
--- Class04 - Generics and Extension Methods/Class04/Class04.Extensions/Program.cs
+++ Class04 - Generics and Extension Methods/Class04/Class04.Extensions/Program.cs
@@ -1,6 +1,12 @@
 ﻿using Class04.Extensions.Helpers;
+using System.Diagnostics;
 
 List<int> integers = new List<int>() { 1, 2, 3, 4, 5 }; 
 List<string> strings = ["str1", "str2", "str3"];
 
-ListHelper.PrintItems<int>(integers); 
+ListHelper.PrintItems<int>(integers);
+ListHelper.PrintItems(strings);
+integers.PrintItems();
+strings.PrintItems();
+
+integers.PrintListInfo(); 
