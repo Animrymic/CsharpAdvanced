1dcdf02bd5a1ecb9a72e7949c7b221f5d9d0991b
Animrymic <trojanovikdragan@gmail.com>
2026-05-20 18:38:06 +0200

    - class 06 code added

---
+++ Class07 - Anonymous Methods and Advanced LINQ/Class06/Class06.AnonymousMethods/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Class06.AnonymousMethods.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Salary { get; set; }
+    public bool IsActive { get; set; }
+}
+
+
--- Class07 - Anonymous Methods and Advanced LINQ/Class06/Class06.AnonymousMethods/Program.cs
+++ Class07 - Anonymous Methods and Advanced LINQ/Class06/Class06.AnonymousMethods/Program.cs
@@ -1,6 +1,83 @@
-﻿List<string> names = ["Alice", "Bob", "Charlie", "John", "Anna"];
+﻿using Class06.AnonymousMethods.Models;
+using System.Security.Cryptography.X509Certificates;
+
+List<string> names = ["Alice", "Bob", "Charlie", "John", "Anna"];
 List<string> empty = [];
 
-string johnName = names.Find(name => name == "John");
+// One line lambda expression to find the name "John" in the list of names
+string johnName = names.Find(name => name == "John")!;
 
-Console.WriteLine(johnName);
+string? john2 = names.Find(name =>
+{
+    if (name == "John")
+    {
+        return true;
+    }
+    return false;
+});
+
+// In JavaScript:
+// let sum = (num1, num2) => num1 + num2;
+// sum(10, 20); // Output: 30
+
+//parameter 1 => int
+//parameter 2 => int
+//return type => int
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\n=============FUNC==============");
+Console.ResetColor();
+
+//var sum = (num1, num2) => num1 + num2;
+
+//Example of a Func with 2 parameters
+Func<int, int, int> sum = (num1, num2) => num1 + num2;
+int sumResult = sum(10, 20);
+Console.WriteLine(sumResult);
+
+//Example of a Func with no parameters
+Func<bool> isNamesEmpty = () => names.Count == 0;
+Console.WriteLine("Is list empty: " + isNamesEmpty());
+
+//Example of a Func with 1 parameter
+Func<List<string>, bool> isListEmpty = (list) => list.Count == 0;
+Console.WriteLine("The list names is: " + isListEmpty(names));
+Console.WriteLine("The list empty is: " + isListEmpty(empty));
+
+//Example of a Func with 2 parameters
+Func<int, int, bool> isLarger = (num1, num2) =>
+{
+    if (num1 > num2)
+    {
+        return true;
+    }
+    return false;
+};
+
+Console.WriteLine("Is 20 larger than 10: " + isLarger(20, 10));
+Console.WriteLine("Is 5 larger than 10: " + isLarger(5, 10));
+
+//Example of a Func with 4 parameters
+Func<int, string, double, bool, string> getUserInfo = (age, name, height, isStudent) =>
+{
+    return $"{name} is {age} years old, {height} meters tall, and {(isStudent ? "Yes" : "No")}";
+};
+Console.WriteLine(getUserInfo(25, "Alice", 1.65, true));
+Console.WriteLine(getUserInfo(30, "Bob", 1.80, false));
+
+// ===> Example of a Func that uses class as a type
+Func<Person, string> getPersonName = person =>
+{
+    return person.Name;
+};
+Person bob = new Person {Name = "Bob"};
+Console.WriteLine(getPersonName(bob));
+
+// ===> Example of a Func that only prints Hello World
+Action printHello = () =>
+{
+    Console.WriteLine("Hello");
+};
+
+
+
