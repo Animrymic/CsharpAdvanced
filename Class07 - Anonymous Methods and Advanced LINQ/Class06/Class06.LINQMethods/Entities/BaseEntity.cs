d0a9ba2afea137e02b2f258ed98fc4aaae1fcd45
Animrymic <trojanovikdragan@gmail.com>
2026-05-20 19:39:33 +0200

    -- helpers added and other classes

---
+++ Class07 - Anonymous Methods and Advanced LINQ/Class06/Class06.LINQMethods/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace LinqMethods.Entities;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public abstract string GetInfo();
+}
---
+++ Class07 - Anonymous Methods and Advanced LINQ/Class06/Class06.LINQMethods/Helpers/ConsoleHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace LinqMethods.Helpers;
+
+public static class ConsoleHelper
+{
+    public static void PrintInColor(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+}
