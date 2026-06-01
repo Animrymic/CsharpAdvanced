2944fa2d4a798af1fc5756f813407eeb1b3abae9
Animrymic <trojanovikdragan@gmail.com>
2026-06-01 18:27:40 +0200

    -aded

---
+++ Class06 - TaxiManager9000 pt2/TaxiManager9000/TaxiManager9000.App/TaxiManagerUI.cs
@@ -0,0 +1,25 @@
+﻿using TaxiManager9000.Helper; 
+
+namespace TaxiManager9000.App
+{
+    internal class TaxiManagerUI
+    {
+
+
+        public void InitApp()
+        {
+            #region Login Menu
+
+            try
+            {
+                ConsoleHelper
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            #endregion
+        }
+    }
+}
