c121d497d566efa60c7f64b3b27a89cf8d41e1eb
Animrymic <trojanovikdragan@gmail.com>
2026-06-03 18:39:02 +0200

    demo code added

---
+++ Class10 - Disposing and Managing Resources/Class10/Class10.Dispose&ManageResources/Program.cs
@@ -0,0 +1,44 @@
+﻿#region Value and Reference Types
+using Class10.Dispose_ManageResources;
+using Class10.DisposeAndManageResources.Helpers;
+
+ExtendedConsole.PrintInColor("\n================ STACK and HEAP ================\n", ConsoleColor.Dark    Cyan);
+ExtendedConsole.PrintInColor("\n============= VALUE TYPES and REFERENCE TYPES =============\n", ConsoleColor.Cyan);
+
+Console.WriteLine("\n===== Value Types =====\n");
+int num1 = 10;
+int num2 = num1; // num2 gets a copy of the value in num1
+num2 = 20; // Changing num2 does not affect num1
+Console.WriteLine($"num1: {num1}, num2: {num2}");
+
+Console.WriteLine("\n===== Reference Types =====\n");
+List<int> ints1 = new List<int> { 1, 2, 3 };
+List<int> ints2 = ints1; // ints2 references the same list as ints1
+ints2[1] = 100; // Changing ints2 affects ints1 because they reference the same list
+
+ints1.ForEach(Console.WriteLine); // Output: 1, 100, 3
+ints2.ForEach(Console.WriteLine); // Output: 1, 100, 3
+
+// Example User 
+User john = new User("John", "Malkovic", 70);
+User john2 = john; // john2 references the same User object as john
+john2.Age = 32; // Changing john2's Age also changes john's Age because they reference the same object
+
+User john3 = john2;
+john3.LastName = "Doe";
+
+john.PrintInfo(); // Output: John Doe, Age: 32
+john2.PrintInfo(); // Output: John Doe, Age: 32
+john3.PrintInfo(); // Output: John Doe, Age: 32
+
+Console.WriteLine("\n===== Strings =====\n");
+
+// Example *String*
+string stringOne = "Hello";
+string stringTwo = stringOne; // stringTwo references the same string as stringOne
+stringTwo = "World"; // Changing stringTwo does not affect stringOne because strings are immutable
+
+Console.WriteLine(stringOne);
+Console.WriteLine(stringTwo);
+
+#endregion
---
+++ Class10 - Disposing and Managing Resources/Class10/Nullables/Program.cs
@@ -0,0 +1,22 @@
+﻿using Class10.DisposeAndManageResources.Helpers;
+using Nullables;
+
+ExtendedConsole.PrintInColor("============= NULLABLES =============\n", ConsoleColor.Cyan);
+
+//int number = null; This will cause a compile-time error because int is a non-nullable value type
+
+int? number = null; // This is a nullable int, so it can hold a null value
+
+double decimalNumber = 465.545; // Non-nullable double
+double? decimalNum = null; // Nullable double
+
+bool? isTrue = null; // Nullable bool
+
+DateTime? date = null; // Nullable DateTime
+
+
+string text = null;
+string? text2 = null;
+
+Person bob = new Person();
+bob.Score = null;
