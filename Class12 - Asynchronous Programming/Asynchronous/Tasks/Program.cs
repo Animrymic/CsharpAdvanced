a736fbf0df78babb1c22c15388b9fd042592e408
Animrymic <trojanovikdragan@gmail.com>
2026-06-08 20:33:03 +0200

    class12 async demo code added

---
+++ Class12 - Asynchronous Programming/Asynchronous/Tasks/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("\n================== TASKS ==================\n\n");
+
+Console.WriteLine("===== Task Status Lifecycle =====");
+
+//Task myTask = new Task(() => { }); Old way of creating a task
+//myTask.Start();
+
+//1) Task without return value
+Task myTask = Task.Run(() => 
+{ 
+    Thread.Sleep(2000);
+    Console.WriteLine("Running after 2000 ms");
+});
+
+Console.WriteLine($"Right after start: {myTask.Status}");
+Thread.Sleep(500);
+Console.WriteLine($"While running: {myTask.Status}");
+myTask.Wait();
+Console.WriteLine($"After completion: {myTask.Status}");
+
+//2) Task with return value integer
+Task<int> valueTask = Task.Run(() =>
+{
+    Thread.Sleep(1500);
+    return 300;
+});
+
+Console.WriteLine(valueTask.Result);
+Console.WriteLine("Status of valueTask: " + valueTask.Status);
+
+Console.WriteLine("====== 20 Tasks ======\n");
+
+Random rnd = new Random();
+for (int i = 1; i <= 20; i++)
+{
+    int temp = i;
+    Task.Run(() =>
+    {
+        int delay = rnd.Next(500, 2000);
+        Thread.Sleep(delay);
+        Console.WriteLine($"Task {temp} done after {delay} ms. [ThreadID : {Thread.CurrentThread.ManagedThreadId}]");
+    });
+}
+
+Console.WriteLine($"Main Thread Id: {Thread.CurrentThread.ManagedThreadId}\n");
+
+
+Console.ReadLine();
---
+++ Class12 - Asynchronous Programming/Asynchronous/Threads/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("==================== THREADS ====================\n\n");
+
+Console.WriteLine($"Main Thread Id: {Thread.CurrentThread.ManagedThreadId}\n");
+
+// Synchronous 
+void SendMessages()
+{
+    Console.WriteLine("Getting Ready...");
+    Thread.Sleep(2000);
+
+    Console.WriteLine("First message arrived!");
+    Thread.Sleep(2000);
+
+    Console.WriteLine($"Main Thread Id: {Thread.CurrentThread.ManagedThreadId}\n");
+
+    Console.WriteLine("Second message arrived!");
+    Thread.Sleep(2000);
+
+    Console.WriteLine("Third message arrived!");
+    Console.WriteLine("All messages are received!");
+}
+
+//SendMessages();
+
+// Asynchronous
+void SendMessagesWithThread()
+{
+    Console.WriteLine("Getting Ready...");
+    Random rnd = new Random();
+
+    Thread t1 = new Thread(() =>
+    {
+       int delay = rnd.Next(500, 2000);
+        Thread.Sleep(delay);
+        Console.WriteLine($"First message arrived after delay {delay} ms![ThreadID: {Thread.CurrentThread.ManagedThreadId}]");
+    });
+
+    Thread t2 = new Thread(() =>
+    {
+        int delay = rnd.Next(500, 2000);
+        Thread.Sleep(delay);
+        Console.WriteLine($"Second message arrived after delay {delay} ms![ThreadID: {Thread.CurrentThread.ManagedThreadId}]");
+    });
+
+    Thread t3 = new Thread(() =>
+    {
+        int delay = rnd.Next(500, 2000);
+        Thread.Sleep(delay);
+        Console.WriteLine($"Third message arrived after delay {delay} ms![ThreadID: {Thread.CurrentThread.ManagedThreadId}]");
+    })
+    { Name = "Our Thread 3" };
+
+    t1.Start(); t2.Start(); t3.Start();
+
+    Console.WriteLine($"Main Thread Id: {Thread.CurrentThread.ManagedThreadId}\n");
+    Console.WriteLine("All messages are received! (or not yet...)");
+}
+
+SendMessagesWithThread();
+
+
+
+
+Console.ReadLine();
